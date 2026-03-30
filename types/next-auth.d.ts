1f25507d4974eae6b411f2b43319c1594713ef19
Juniordel <juniordel@gmail.com>
2026-03-30 16:00:55 -0300

    fix: add next-auth type declarations

--- types/next-auth.d.ts
+++ types/next-auth.d.ts
@@ -3,20 +3,36 @@ import NextAuth from 'next-auth'
 declare module 'next-auth' {
   interface Session {
     user: {
-      id: string
-      name: string
-      email: string
-      role: string
-      workspaceId: string
-      workspaceNome: string
+      id:              string
+      name:            string
+      email:           string
+      role:            string
+      workspaceId:     string
+      workspaceNome:   string
+      workspaceAtivo:  boolean
+      primeiroLogin:   boolean
     }
+  }
+
+  interface User {
+    id:              string
+    name:            string
+    email:           string
+    role:            string
+    workspaceId:     string
+    workspaceNome:   string
+    workspaceAtivo:  boolean
+    primeiroLogin:   boolean
   }
 }
 
 declare module 'next-auth/jwt' {
   interface JWT {
-    role: string
-    workspaceId: string
-    workspaceNome: string
+    id:              string
+    role:            string
+    workspaceId:     string
+    workspaceNome:   string
+    workspaceAtivo:  boolean
+    primeiroLogin:   boolean
   }
 }
